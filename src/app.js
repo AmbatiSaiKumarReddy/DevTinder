3e1c4d28855b1835e3439358abad0d999cd2347b
ambatisaikumarreddy@gmail.com <ambatisaikumarreddy@gmail.com>
2024-09-19 22:55:43 -0400

    Error Handling and wild card error handling

--- src/app.js
+++ src/app.js
@@ -13,9 +13,18 @@ app.get("/admin/getAdmin",(req,res,next)=>{
     
     
     console.log('Admin Data sent')
-    res.send("Admin Data Sent")
+    //Error Handling through try catch.
+    //Fetch data from database
+    try{ 
+    throw new Error("Cannot fetch data")
+    }
+    catch(err){
+        res.status(500).send(err);
+    }
+      res.send("Admin Data Sent")
     
 })
+
 
 app.delete("/admin/deleteAdmin",(req,res,next)=>{
     console.log('Admin deleted')
@@ -26,7 +35,16 @@ app.delete("/admin/deleteAdmin",(req,res,next)=>{
 //User
 app.get("/user",(req,res,next)=>{
     console.log('User Data Logged');
+    //Error handle will occur at wild card 
+    //Fetch data from database
+    throw new Error("Cannot fetch user data");
     res.send("User Data sent");
+})
+
+app.use('/',(err,req,res,next)=>{
+    console.log('Wild card Middleware')
+    console.log(err.message);
+    res.status(502).send(err.message);
 })
 
 app.listen(port,()=>{
