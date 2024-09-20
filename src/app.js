7324a96238b002138139684d0adacd42b79062e7
ambatisaikumarreddy@gmail.com <ambatisaikumarreddy@gmail.com>
2024-09-20 04:09:26 -0400

    Get and Post Api's

--- src/app.js
+++ src/app.js
@@ -8,18 +8,46 @@ const User=require('./models/user')
 const app=express();
 const port=7777;
 
-app.post("/user",async (req,res,next)=>{
-    const newUser={
-        firstName:"Akshay",
-        lastName:"Saini",
-        emailId:"akshay@saini.com",
-        password:"akshay@123"
-    }
+app.use(express.json())
+
+//Get User By EmailId
+app.get("/user",async(req,res,)=>{
+
     try{
-    const user=new User(newUser);
+        const user=await User.findOne({emailId:req.body.emailId});
+        res.send(user)
+    }
+    catch(error){
+        res.send(500).send("Error fetching data")
+    }
+
+})
+
+//Feed Api- Get All Users from Database
+
+app.get("/feed",async(req,res,)=>{
+
+    try{
+        const user=await User.find({});
+        console.log(user)
+        res.send(user)
+    }
+    catch(error){
+        res.send(500).send("Error fetching data")
+    }
+
+})
+
+
+app.post("/user",async (req,res,next)=>{
+    console.log(req.body);
+    
+    try{
+    const user=new User(req.body);
     await user.save();
     res.send("User created successfully")}
     catch(error){
+        console.log(error.message)
         res.status(400).send("Failed to create user")
     }
 
